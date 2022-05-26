86e5331b687e78fe4c7bc00f2699526facc0019f
FloxxStudio <83787802+yLuigizinDev@users.noreply.github.com>
2022-05-26 12:02:02 -0300

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,5 +1,5 @@
 if syn then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/yLuigizinDev/idklol/main/normal_bs", true))()
 else
-  loadstring(game:HttpGet("https://github.com/yLuigizinDev/idklol/blob/main/bs_krnl.lua?raw=true", true))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/yLuigizinDev/idklol/main/bs_krnl.lua", true))()
 end
